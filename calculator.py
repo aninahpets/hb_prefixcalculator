8acf3c0c68275b274646cd272e087b7f01be6135
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 16:51:13 -0700

    Added unlimited user inputs for addition and multiplication functions.

--- calculator.py
+++ calculator.py
@@ -12,33 +12,51 @@ while True:
     user_input = raw_input("Please enter a calculation: ")
 #     read input
     user_input = user_input.lower()
+#put user_input into lowercase letters
     user_input = user_input.split(" ")
+#splits user_input into separate strings in a single list - "tokenizes"
     for i in range(1,len(user_input)):
+#iterates through the list starting with index 1 (skipping operator)
         user_input[i] = int(user_input[i])
-#     tokenize input
+#changes type from string to integer
     if user_input[0] == "q":
 #     if the first token is 'q'
         break
 #         quit
     elif user_input[0] == "+":
-        print add(user_input[1], user_input[2])
+        add_value = 0
+        for i in range(1, len(user_input)):
+            add_value = add(add_value, user_input[i])
+        print add_value
+            # print current_value
+#if user_input starts with "+", call add function and print output
     elif user_input[0] == "-":
         print subtract(user_input[1], user_input[2])
+#if user_input starts with "-", call subtract function and print output
     elif user_input[0] == "*":
-        print multiply(user_input[1], user_input[2])
+        multiply_value = 1
+        for i in range(1, len(user_input)):
+            multiply_value = multiply(multiply_value, user_input[i])
+        print multiply_value        
+#if user_input starts with "*", call multiply function and print output
     elif user_input[0] == "/":
         print divide(user_input[1], user_input[2])
+#if user_input starts with "/", call divide function and print routput
     elif user_input[0] == "square":
         print square(user_input[1])
+#if user_input starts with "square", call square function and print output
     elif user_input[0] == "cube":
         print cube(user_input[1])
+#if user_input starts with "cube", call cube function and print output
     elif user_input[0] == "power":
         print power(user_input[1], user_input[2])
+#if user_input starts with "power", call power function and print output
     elif user_input[0] == "mod":
         print mod(user_input[1], user_input[2])
+#if user_input starts with "mod", call mod function and print output
     else:
         print "That was not a valid choice. Please try again."
-
+#provides response if user inputs invalid selection
 
 
 
